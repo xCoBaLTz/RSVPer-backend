dffe6ad81b1c8c5c79dd6e48682af330d1fa5494
Seanan Chabra <seanan.chabra@freshbooks.com>
2022-07-28 19:59:05 -0400

    Remove sql files

--- sql/fill_tables.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (email) VALUES ('seananchabra@hotmail.com');
-
-INSERT INTO invites (user_id, first_name, last_name) VALUES
-((select id from users where email = 'seananchabra@hotmail.com'), 'Seanan', 'Chabra'),
-((select id from users where email = 'seananchabra@hotmail.com'), 'Nirmala', 'Chabra'),
-((select id from users where email = 'seananchabra@hotmail.com'), 'Prabhjot', 'Mudhar'),
-((select id from users where email = 'seananchabra@hotmail.com'), 'Jason', 'Chabra');
